4f5e7a95bede5c51f03101a1afc2e724ec396d11
Bekzod <bekzod.abdiraxmonov@outlook.com>
2025-02-07 02:14:12 +0500

    INFRA: Upload Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Sonni kiriting: ");
+        int x = int.Parse(Console.ReadLine()); // Butun sonni kiritish
+        Console.Write("So'zni kiriting: ");
+        string str = Console.ReadLine(); // Matnni kiritish
+
+        // Tekshiruv: x str ning uzunligidan katta yoki yo'q
+        string result = 
+                        x > str.Length 
+                            ? str.ToUpper() 
+                            : str.ToLower();
+
+        Console.WriteLine("Natija: " + result);
